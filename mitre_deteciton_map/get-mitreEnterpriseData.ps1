f9d444e5343c03eef51c71e16552f6dce7ecb16b
red1turtle <raphael.francoeur@gmail.com>
2026-03-02 08:54:30 -0600

    Add files via upload

--- mitre_deteciton_map/get-mitreEnterpriseData.ps1
+++ mitre_deteciton_map/get-mitreEnterpriseData.ps1
@@ -1,3 +1,3 @@
 #will create a temp dir and grab enterprise-attack-18.1
 New-Item c:\temp -ItemType Directory -Force | Out-Null
-Invoke-RestMethod "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/refs/heads/master/enterprise-attack/enterprise-attack-18.1.json" -OutFile c:\temp\enterprise_attack.json
+Invoke-RestMethod "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/refs/heads/master/enterprise-attack/enterprise-attack-18.1.json" -OutFile .\enterprise_attack.json
